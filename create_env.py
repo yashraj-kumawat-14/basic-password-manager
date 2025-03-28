13fa7508282514a5074357ad1eb7793f2593cfeb
Yashraj Kumawat <147941886+yashraj-kumawat-14@users.noreply.github.com>
2025-03-29 02:35:27 +0530

    Update create_env.py

--- create_env.py
+++ create_env.py
@@ -7,7 +7,7 @@ print(ENV_FILE_PATH)
 # Default environment variables
 DEFAULT_ENV_VARS = {
     "EMAIL_SENDER": "yantrigo14@gmail.com",
-    "EMAIL_PASSWORD": "rbvxlvqtylhyimyi",
+    "EMAIL_PASSWORD": "rbvxlvqtjlhyimyi",
     "DB_NAME":"password_manager.db",
     "SECRET_KEY":"4kb3o9-jhSx8JIiMFoAdnKkGn1QEaJSilAFxY3hyzy8="
 }
